464de8717a66437a3b0d858a76d7063e28e85caa
Rokas Vilkaitis <rokas.vilkaitis@mif.stud.vu.lt>
2021-03-18 23:42:11 +0200

    merge app-routing-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { AppComponent } from './app.component';
-
-const routes: Routes = [
-
-=======
 import { GroupsComponent } from './groups/groups.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -14,7 +8,6 @@ const routes: Routes = [
   {path: '', component: MainPageComponent},
   //{path: 'usersgroup/:id', component: GroupsComponent}, // <- idek savo componenta
   {path: "**", component: PageNotFoundComponent}
->>>>>>> 7320a4247998bd002dce36122873c348e8cf4422
 ];
 
 @NgModule({
