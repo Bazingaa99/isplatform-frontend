a6c9d312c5feb1a084fafbaf4d7ea06ebfb82bd3
Valdemaras Danys <valdemaras.danys@mif.stud.vu.lt>
2021-03-24 02:43:14 +0200

    Arrows fixed

--- src/app/group-creation-dialog/group-creation-dialog.component.ts
+++ src/app/group-creation-dialog/group-creation-dialog.component.ts
@@ -50,7 +50,6 @@ export class GroupCreationDialogComponent {
 
   onSubmit(): void {
     this.closeDialog();
-    console.log(this.userId);
     this.usersGroup = {
       admin_id: this.userId,
       name: this.userCreationForm.get('name').value,
@@ -66,7 +65,7 @@ export class GroupCreationDialogComponent {
         })
       },
       (error: HttpErrorResponse) => {
-        this.snackBar.open("Couldn't add item. Please try again.","✓",{
+        this.snackBar.open("Couldn't create a group. Please try again.","✓",{
           duration: 400000000000000,
           panelClass: ['red-snackbar']
         })
