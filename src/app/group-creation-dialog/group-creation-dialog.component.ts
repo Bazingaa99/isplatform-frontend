990f60ee9e223684aa8bf097085f8142934b8b7a
Valdemaras Danys <valdemaras.danys@mif.stud.vu.lt>
2021-03-24 02:47:55 +0200

    Fixed group creation function name

--- src/app/group-creation-dialog/group-creation-dialog.component.ts
+++ src/app/group-creation-dialog/group-creation-dialog.component.ts
@@ -48,7 +48,7 @@ export class GroupCreationDialogComponent {
     this.dialog.close(GroupCreationDialogComponent);
   }
 
-  onSubmit(): void {
+  onSubmitGroup(): void {
     this.closeDialog();
     this.usersGroup = {
       admin_id: this.userId,
