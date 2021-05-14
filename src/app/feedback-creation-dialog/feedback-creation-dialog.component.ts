31507c2ab5e6b7f9ab29e7a8f4b88b2285c7afd8
Valdemaras Danys <valdemaras.danys@mif.stud.vu.lt>
2021-05-14 11:39:13 +0300

    Feedback form

--- src/app/feedback-creation-dialog/feedback-creation-dialog.component.ts
+++ src/app/feedback-creation-dialog/feedback-creation-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback } from '../shared/feedback';
 
 @Component({
   selector: 'feedback-creation-dialog',
@@ -7,10 +9,47 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./feedback-creation-dialog.component.scss']
 })
 export class FeedbackCreationDialogComponent implements OnInit {
+  feedbackCreationForm: FormGroup;
+  userRated = true;
+  feedback: Feedback;
 
-  constructor(public dialog: MatDialogRef<FeedbackCreationDialogComponent>) { }
+  constructor(public dialog: MatDialogRef<FeedbackCreationDialogComponent>,
+              private fb: FormBuilder) {
+    this.feedbackCreationForm = this.setForm();
+   }
 
   ngOnInit(): void {
+  }
+
+  setForm() {
+    return this.fb.group({
+      feedbackText: ['', [
+        Validators.maxLength(100),
+      ]],
+    });
+  }
+
+  onCreateFeedback(): void{
+    var stars = document.getElementsByTagName('input');
+    let starsCount;
+    for (var i=0; i<stars.length; i++) {
+      if(stars[i].type == 'radio' && stars[i].checked){
+        this.userRated = true;
+        console.log("ivyko");
+        starsCount = 6-i;
+        break;
+      }
+      if (i==(stars.length-1) && stars[i].type == 'radio' && !stars[i].checked) {
+        this.userRated = false;
+      }
+    }
+    if(this.userRated){
+      this.closeDialog();
+      this.feedback = {
+       feedbackMessage: this.feedbackCreationForm.get('feedbackText').value,
+       starsCount: starsCount,
+      }
+    }
   }
 
   closeDialog(): void {
---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,4 @@
+export interface Feedback {
+    feedbackMessage: string;
+    starsCount: number;
+}
