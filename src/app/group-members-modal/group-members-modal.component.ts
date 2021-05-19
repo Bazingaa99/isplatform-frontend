f5bb381d4adbb2a288bb5f5ca00f25a1fea9ff23
Rokas Vilkaitis <rokas.vilkaitis@mif.stud.vu.lt>
2021-05-19 03:56:37 +0300

    add pagination to member display

--- src/app/group-members-modal/group-members-modal.component.ts
+++ src/app/group-members-modal/group-members-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
 import { Router } from '@angular/router';
 import { User } from '../shared/user';
 
@@ -10,11 +11,15 @@ import { User } from '../shared/user';
 })
 export class GroupMembersModalComponent implements OnInit {
   users: User[]
+  usersLength: number;
   currentUser: string;
+  pageSlice: User[];
   constructor(public router: Router,
               public dialog: MatDialogRef<GroupMembersModalComponent>,
               @Inject(MAT_DIALOG_DATA) public data: User[]) {
     this.users = data;
+    this.usersLength = this.users.length;
+    this.pageSlice = this.users.slice(0, 6);
   }
 
   ngOnInit(): void {
@@ -24,5 +29,16 @@ export class GroupMembersModalComponent implements OnInit {
   goToUserProfile(link: string){
     this.dialog.close(GroupMembersModalComponent);
     this.router.navigateByUrl(link);
+  }
+
+  public onPageChange(event: PageEvent){
+    const startIndex = event.pageIndex * event.pageSize;
+    let endIndex = startIndex + event.pageSize;
+
+    if(endIndex > this.users.length){
+      endIndex = this.users.length;
+    }
+
+    this.pageSlice = this.users.slice(startIndex, endIndex);
   }
 }
